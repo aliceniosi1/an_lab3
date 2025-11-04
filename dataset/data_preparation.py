ff416e94d5b8e71f7776cd905f0efdf6f7b30fe3
aliceniosi1 <s339225@studenti.polito.it>
2025-11-04 21:40:22 +0100

    modify: dataloader

--- dataset/data_preparation.py
+++ dataset/data_preparation.py
@@ -1,7 +1,9 @@
 import os
+import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
 def data_preparation(batch_size=64):
     data_root = os.path.join("data", "tiny-imagenet", "tiny-imagenet-200")
 
@@ -9,20 +11,26 @@ def data_preparation(batch_size=64):
     val_dir = os.path.join(data_root, "val")
 
     # Trasformazioni base
-    transform = transforms.Compose([
-        transforms.Resize((64, 64)),
-        transforms.ToTensor(),
+    transform = T.Compose([
+        T.Resize((224, 224)),  # adattato per reti come ResNet
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]),
     ])
 
     # Dataset
-    train_dataset = datasets.ImageFolder(train_dir, transform=transform)
-    val_dataset = datasets.ImageFolder(val_dir, transform=transform)
+    tiny_imagenet_dataset_train = ImageFolder(root=train_dir, transform=transform)
+    tiny_imagenet_dataset_val = ImageFolder(root=val_dir, transform=transform)
 
     # DataLoader
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(
+        tiny_imagenet_dataset_train, batch_size=batch_size,
+        shuffle=True, num_workers=8
+    )
+    val_loader = torch.utils.data.DataLoader(
+        tiny_imagenet_dataset_val, batch_size=batch_size,
+        shuffle=False, num_workers=8
+    )
 
-    print(f"✅ Loaded {len(train_dataset)} training images and {len(val_dataset)} validation images.")
-
+    print(f"✅ Loaded {len(tiny_imagenet_dataset_train)} training images and {len(tiny_imagenet_dataset_val)} validation images.")
     return train_loader, val_loader
-
