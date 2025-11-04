b04c0c299669e29aad8f2511887976ee98ebe43c
aliceniosi1 <s339225@studenti.polito.it>
2025-11-04 21:26:01 +0100

    modify: custom_Model

--- models/custom_net.py
+++ models/custom_net.py
@@ -1,17 +1,24 @@
-# models/custom_net.py
-import torch.nn as nn
-
+import torch
+from torch import nn
 class CustomNet(nn.Module):
-    def __init__(self, num_classes=200):
-        super(CustomNet, self).__init__()
+    def __init__(self):
+        super().__init__()  # <--- versione Python 3
+        # Convolutional layers
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)  # 64 -> 32
+
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc = nn.Linear(128 * 112 * 112, num_classes)
+        self.pool2 = nn.MaxPool2d(2, 2)  # 32 -> 16
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)  # 16 -> 8
+
+        self.fc1 = nn.Linear(256 * 8 * 8, 200)
 
     def forward(self, x):
-        x = self.pool(self.conv1(x).relu())
-        x = self.pool(self.conv2(x).relu())
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.pool1(self.conv1(x).relu())
+        x = self.pool2(self.conv2(x).relu())
+        x = self.pool3(self.conv3(x).relu())
+        x = x.flatten(start_dim=1)
+        x = self.fc1(x)
         return x
